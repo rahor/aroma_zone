8a4c27e8ae97f4eb8653f577f5944e668a81d03b
Rhor <hor.raphaell@gmail.com>
2018-12-21 09:50:37 +0100

    html+css js imgclean jour5 v1

--- js/main.js
+++ js/main.js
@@ -1,44 +1,28 @@
 // Gestion du menu sticky
-window.onscroll = function() {myFunction()};
-      var header = document.querySelectorAll('ul');
-      console.log(header[2]);
-      
-      var sticky = header[2].offsetTop;
-      
-      function myFunction() {
-        if (window.pageYOffset > sticky) {
-          header[2].classList.add("sticky");
-        } else {
-          header[2].classList.remove("sticky");
-}
-        }
-
+  var header = document.querySelectorAll('ul');
+  console.log(header[2]);
+  var sticky = header[2].offsetTop;
+  window.onscroll = ()=>{
+      if (window.pageYOffset > sticky) header[2].classList.add("sticky");
+      else header[2].classList.remove("sticky");
+  } 
+ 
 //Gestion de la quantité
-var nombre = document.querySelectorAll('input')[1];
-nombre.value = '1';
+  var nombre = document.querySelectorAll('input')[1];
+  nombre.value = '1';
 
-document.getElementById('plus').addEventListener('click', () => {
-  if (nombre.value < 99) {
-      nombre.value++;
-  }
-});
+  document.getElementById('plus').addEventListener('click', () => {
+    if (nombre.value < 99) nombre.value++;
+  });
 
-document.getElementById('moin').addEventListener('click', () => {
-  if (nombre.value > 0) {
-      nombre.value--;
-  }
-});
+  document.getElementById('moin').addEventListener('click', () => {
+    if (nombre.value > 0) nombre.value--;
+  });
 
 //Affichage du coeur
-var coeur = document.querySelectorAll('i')[1];
-console.log(coeur);
-
-coeur.addEventListener('click',()=>{
-  // console.log(coeur.style.color);
-  if(coeur.style.color == 'grey'||coeur.style.color == ''){
-  coeur.style.color = '#DF4343';}
-  else {console.log(coeur.style.color);
-  coeur.style.color = 'grey';
-  }
-});
+  var coeur = document.querySelectorAll('i')[1];
+  coeur.addEventListener('click',()=>{
+    if(coeur.style.color == 'grey'||coeur.style.color == '') coeur.style.color = '#DF4343';
+    else coeur.style.color = 'grey';
+  });
   
